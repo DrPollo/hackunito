81dfc7f955fda94cbdfc7e63a693e78533b4719a
Alessio <alessio.p.antonini@gmail.com>
2013-11-26 19:09:58 +0100

    Update loop-projects.php
    Project meta

--- wp-content/themes/cbox-projectpress/templates/loops/loop-projects.php
+++ wp-content/themes/cbox-projectpress/templates/loops/loop-projects.php
@@ -23,6 +23,10 @@ $args = array(
 $wp_query = new WP_Query( $args );
 //$wp_query->query('posts_per_page=8'.'&paged='.$paged);
 while ($wp_query->have_posts()) : $wp_query->the_post();
+	// group variables 
+	$group_id = get_post_custom_values('_group_node');
+	$g_id = $group_id[0];
+	$group = groups_get_group( array( 'group_id' => $g_id ) );
 ?>
 		<!-- post -->
 		<article class="post" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -33,50 +37,88 @@ while ($wp_query->have_posts()) : $wp_query->the_post();
 			<div class="post-content">
 				<!-- post title -->
 				<h2 class="post-title">
-					<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e( 'Permanent Link to', infinity_text_domain ) ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+					<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e( 'Permanent Link to', 'infinity_text_domain'); ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
 					<?php edit_post_link(' ✍','',' ');?>
 				</h2>
 				
-				<h1>
-				<?php
-					$group_id = get_post_custom_values('_group_node');
-					$g_id = $group_id[0];
-					$group = groups_get_group( array( 'group_id' => $g_id ) );
-					$g_name = $group -> name;
-					echo $g_name;
-				?>
-				</h1>
+				<div class="project-meta-fields">
+					<span class="project-meta"><?php _e('Followers: ','infinity_text_domain'); ?>
+						<span class="group-couter"><?php echo $group -> total_member_count; ?> </span>
+					</span>
+					<span class="separatore">&nbsp;</span>
+					<span class="project-meta"><?php _e('Last update: ','infinity_text_domain'); ?>
+						<span class="group-couter"><?php 
+							$last_activity = $group -> last_activity; 
+							$newDate = date(__('H:i d/m/Y','infinity_text_domain'), strtotime($last_activity));
+							echo $newDate;
+						?> </span>
+					</span>
+				</div>
 				
-				<h1>
+				<div>
 				<?php
-					$g_description = $group -> description;
-					echo $g_description;
-				?>
-				</h1>
-				
-				<h1>
-				<?php
-					$g_admins = $group -> admins; // ARRAY degli amministratori
-					foreach ($g_admins as $admin){
-						$a_name = $admin -> user_nicename;
-					    echo $a_name;
-					    }
-				?>
-				</h1>
-				
-				<div> Membri del gruppo:<br/>
-				<?php
-				$members = groups_get_group_members( $g_id, false,false,false);
-					foreach ($members as $member){
-						foreach ($member as $m){
-							$name = $m->display_name;
-							$id = $m->ID;
-							echo $name;
-							echo bp_core_fetch_avatar (array('item_id' => $id, 'type' => 'full')).'<br/>';
-							}	
-					    }
+					/* project description */
+					the_excerpt();
 				?>
 				</div>
+				
+				<?php
+				/* groups and project share the same name */
+				/* if a group can have more than one project you want to show also the group name */
+					// echo '<h3>';
+					//$g_name = $group -> name;
+					//echo $g_name;
+					// echo '</h3>';
+				/* group description */
+					//$g_description = $group -> description;
+					//echo $g_description;
+				?>
+				
+				
+				
+				
+				<div>
+					<h5><?php _e('Proposers','infinity_text_domain'); ?></h5>
+				<?php
+					$g_admins = $group -> admins; // ARRAY degli amministratori
+					if(!empty($g_admins)) {
+						echo '<ul>';
+						foreach ($g_admins as $admin){
+							echo '<li>';
+							$a_name = $admin -> user_login;
+							$a_id = $admin->user_id;
+							echo bp_core_fetch_avatar (array('item_id' => $a_id, 'width' => 35));
+							echo $a_name;
+							echo'</li>';
+						}
+						echo '</ul>';
+					}
+				?>
+				</div>
+				
+				
+				<?php
+					$g_mods = $group -> mods; // ARRAY degli amministratori
+					if(!empty($g_mods)) { ?>
+						<div>
+							<h5><?php _e('Controbutors','infinity_text_domain'); ?></h5>
+								<ul>
+								<?php
+								foreach ($g_mods as $mod){
+									echo '<li>';
+									$m_name = $mod -> user_login;
+									$m_id = $mod->user_id;
+									echo bp_core_fetch_avatar (array('item_id' => $m_id, 'width' => 35));
+									echo $m_name;
+									echo '</li>';
+								} ?>
+								</ul>
+						</div>
+						<?php
+					}
+				?>
+				
+				
 				
 				
 				<?php
